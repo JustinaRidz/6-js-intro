264e2293c70fc487d4f7dd1a49e574e2650ec576
Justina <justina.ridz@gmail.com>
2020-04-28 01:41:59 +0300

    Initial commit and variable init.

---
+++ js/variables.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const manoVardas = 'Justina';
+const akiuSkaicius = 2;
+const turiGalva = true; 
+const mokaSkraidyti = false;
+const petriukoPazymiai = [];
+const marytesPazymiai = [10, 8, 2];
+const abc = ['a','b','c', 'e', 'f'];
+
+console.log( manoVardas );
+console.log( akiuSkaicius);
+console.log( turiGalva );
+console.log( mokaSkraidyti );
+console.log( petriukoPazymiai );
+console.log( marytesPazymiai );
+console.log( abc );
+
+
+let megstamasSkaicius = 5;
+console.log( 'Megstamas skaicius:', megstamasSkaicius );
+
+megstamasSkaicius = 3;
+console.log( 'Megstamas skaicius:', megstamasSkaicius );
+
+megstamasSkaicius = 7;
+console.log( 'Megstamas skaicius:', megstamasSkaicius );
